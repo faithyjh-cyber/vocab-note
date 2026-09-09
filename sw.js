6ae323d728db90309ec8fd1799705aa0de0b9a06
faithyjh-cyber <faithyjh@cgnmail.net>
2026-09-10 07:28:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
-const CACHE = 'vocab-v1';
+// HTML/JS는 항상 네트워크에서 최신으로 (캐시는 오프라인 대비용)
+const CACHE = 'vocab-v2';
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  // 네트워크 우선, 실패 시 캐시
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        if (e.request.method === 'GET' && res.ok && e.request.url.startsWith(self.location.origin)) {
+    fetch(e.request, { cache: 'no-store' })
+      .then(res => {
+        if (res.ok && e.request.url.startsWith(self.location.origin)) {
           const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          caches.open(CACHE).then(c => c.put(e.request, copy));
         }
         return res;
       })
